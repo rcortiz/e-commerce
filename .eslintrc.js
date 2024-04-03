e9af519d0350cb9e9e6baed2c254b11cea7e2dcf
rcortiz <rcortiz.dev@gmail.com>
2024-04-03 19:28:08 +0800

    implement create-account and login pages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
+const { off } = require("process");
+
 module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    "prettier/prettier": [
+      'error', { endOfLine: 'auto'}
+    ],
+    'no-console': off
+  }
 }
